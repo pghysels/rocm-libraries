489ae3aa7d0dd922966eebd0cfe6da05ce51c84e
Pieter Ghysels <pieter.ghysels@amd.com>
2026-04-17 15:18:59 -0500

    Force usage of origami::select_workgroup_mapping and origami::select_staggeru.

--- projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
+++ projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
@@ -991,8 +991,9 @@ namespace TensileLite
         uint32_t defaultWGMXCCCHUNK = 0;
 
         // Dynamically pick the values
-        if(sizeMapping.streamK != 0 && skgrid != 0 && sizeMapping.workGroupMapping == 0
-           && sizeMapping.workGroupMappingXCC == -1)
+        if(sizeMapping.streamK != 0 && skgrid != 0)
+          // && sizeMapping.workGroupMapping == 0
+          // && sizeMapping.workGroupMappingXCC == -1)
         {
             auto sizes = problem.problemSizes();
             // Try to find cached WGM and WGMXCC and WGMXCCCHUNK
@@ -1098,8 +1099,9 @@ namespace TensileLite
         size_t defaultStaggerUStrideShift = 0;
 
         // Dynamically pick the values
-        if(sizeMapping.streamK != 0 && skgrid != 0 && sizeMapping.workGroupMapping == 0
-           && sizeMapping.workGroupMappingXCC == -1)
+        if(sizeMapping.streamK != 0 && skgrid != 0)
+        // && sizeMapping.workGroupMapping == 0
+        //   && sizeMapping.workGroupMappingXCC == -1)
         {
             auto sizes = problem.problemSizes();
             // Try to find cached StaggerUMapping, StaggerU and StaggerUStrideShift
